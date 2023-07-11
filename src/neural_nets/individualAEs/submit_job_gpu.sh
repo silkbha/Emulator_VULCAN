5dc740792f9d8b76c00aad7b50b7d36c4ce7cc22
Benjamin Silk <silk@student35.strw.leidenuniv.nl>
2023-07-11 14:38:53 +0200

    small change to slurm script

--- src/neural_nets/individualAEs/submit_job_gpu.sh
+++ src/neural_nets/individualAEs/submit_job_gpu.sh
@@ -1,5 +1,5 @@
 sbatch \
-  --job-name=train_MRAE_gpu \
+  --job-name=train_AEs_gpu \
   --partition=gpu-medium \
   --gres=gpu:1 \
   --mem-per-gpu=11G \
@@ -8,5 +8,5 @@ sbatch \
   --time=24:00:00 \
   --mail-user=silk@strw.leidenuniv.nl \
   --mail-type=ALL \
-  --output=train_MRAE_gpu.out \
+  --output=train_AEs_gpu.out \
   $HOME/data1/Emulator_VULCAN/src/neural_nets/individualAEs/train_autoencoders.sh
