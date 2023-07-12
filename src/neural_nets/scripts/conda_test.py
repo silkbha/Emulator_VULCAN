70e440968d9080fa3d2e5f1ec5f025393b5c04f6
Benjamin Silk <silk@student35.strw.leidenuniv.nl>
2023-07-12 17:13:03 +0200

    fdas

--- src/neural_nets/scripts/conda_test.py
+++ src/neural_nets/scripts/conda_test.py
@@ -9,11 +9,10 @@ from datetime import datetime
 import pickle
 
 print(f"PyTorch {torch.__version__}")
+
+print(f'\nCUDA available: {torch.cuda.is_available()}')
 print(f"CUDA {torch.version.cuda}")
 
-# setup pytorch
-device = torch.device(f"cuda:{params['gpu']}" if torch.cuda.is_available() else "cpu")
-print(f'Running on {device}')
-print(f'Device count: {torch.cuda.device_count()}')
+print(f'\nDevice count: {torch.cuda.device_count()}')
 
 print(torch.zeros(1).cuda())
