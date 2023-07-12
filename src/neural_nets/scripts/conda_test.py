e2f64ef01f043c1a3414f60e0fe192edc0470682
Benjamin H. A. Silk <s1850237@nodelogin01.cm.cluster>
2023-07-12 15:09:08 +0200

    cuda cuda

--- src/neural_nets/scripts/conda_test.py
+++ src/neural_nets/scripts/conda_test.py
@@ -9,7 +9,11 @@ from datetime import datetime
 import pickle
 
 print(f"PyTorch {torch.__version__}")
+print(f"CUDA {torch.version.cuda}")
 
 # setup pytorch
 device = torch.device(f"cuda:{params['gpu']}" if torch.cuda.is_available() else "cpu")
 print(f'Running on {device}')
+print(f'Device count: {torch.cuda.device_count()}')
+
+print(torch.zeros(1).cuda())
--- src/neural_nets/scripts/conda_test.sh
+++ src/neural_nets/scripts/conda_test.sh
@@ -3,5 +3,6 @@
 # module load Miniconda3
 # conda init bash
 # conda activate mrp
+nvidia-smi
 
 python /home/s1850237/data1/Emulator_VULCAN/src/neural_nets/scripts/conda_test.py
