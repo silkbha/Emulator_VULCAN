0ea7cedfdc50478b93dc33095463d7bc540b3b21
Benjamin Silk <silk@student35.strw.leidenuniv.nl>
2023-07-12 15:11:20 +0200

    new env

--- src/neural_nets/scripts/conda_test.sh
+++ src/neural_nets/scripts/conda_test.sh
@@ -5,4 +5,7 @@
 # conda activate mrp
 nvidia-smi
 
+source /home/s1825216/miniconda3/etc/profile.d/conda.sh
+conda activate mrp
+
 python /home/s1850237/data1/Emulator_VULCAN/src/neural_nets/scripts/conda_test.py
