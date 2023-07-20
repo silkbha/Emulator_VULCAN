d49247cecdcc8fe9f47219a9bd27fb16bba4e4c3
Benjamin Silk <silk@student35.strw.leidenuniv.nl>
2023-07-20 11:23:43 +0200

    train old core

--- src/neural_nets/individualAEs/MRAE/train_MRAE.sh
+++ src/neural_nets/individualAEs/MRAE/train_MRAE.sh
@@ -4,6 +4,8 @@
 module load Miniconda3
 source /home/s1850237/data1/miniconda3/julius_env_new/bin/activate
 
+# cp -r poly_dataset/ /scratchdata/s1850237/1790125/
+
 nvidia-smi
 
 python /home/s1850237/data1/Emulator_VULCAN/src/neural_nets/individualAEs/MRAE/train_MRAE.py
