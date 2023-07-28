a4c490f48de6c2bd991b74b4ac490db64c250654
Benjamin Silk <silk@student35.strw.leidenuniv.nl>
2023-07-29 01:32:40 +0200

    fix

--- src/visualization/plot_AE_performance/save_AE_performance.py
+++ src/visualization/plot_AE_performance/save_AE_performance.py
@@ -109,8 +109,8 @@ def main(AE_name):
     # setup directories
     script_dir = os.path.dirname(os.path.abspath(__file__))
     MRP_dir = str(Path(script_dir).parents[2])
-    # dataset_dir = os.path.join(MRP_dir, 'data/poly_dataset/dataset_hendrix')
-    dataset_dir = '/scratchdata/s1850237/1790125/poly_dataset/time_series_dataset'
+    dataset_dir = os.path.join(MRP_dir, 'data/poly_dataset/dataset_hendrix')
+    # dataset_dir = '/scratchdata/s1850237/1790125/poly_dataset/time_series_dataset'
     # dataset_dir = os.path.join(MRP_dir, 'data/bday_dataset/time_series_dataset')
     save_model_dir = os.path.join(MRP_dir, 'src/neural_nets/saved_models_final')
 
